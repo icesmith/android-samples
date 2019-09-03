44bb79118c601676d4a9bf032e8180e00c13881c
Valeriy Katkov <cppmaster@yandex.ru>
2019-09-03 15:03:00 +0300

    CallDelegate is forced to be overriden

--- RetrofitSuspendingCallAdapter/src/main/java/com/icesmith/sample/MainVM.kt
+++ RetrofitSuspendingCallAdapter/src/main/java/com/icesmith/sample/MainVM.kt
@@ -36,41 +36,44 @@ abstract class CallDelegate<TIn, TOut>(
     protected val proxy: Call<TIn>
 ) : Call<TOut> {
     override fun execute(): Response<TOut> = throw NotImplementedError()
-    override fun enqueue(callback: Callback<TOut>): Unit = throw NotImplementedError()
-    override fun clone(): Call<TOut> = throw NotImplementedError()
+    override final fun enqueue(callback: Callback<TOut>) = enqueueImpl(callback)
+    override final fun clone(): Call<TOut> = cloneImpl()
 
     override fun cancel() = proxy.cancel()
     override fun request(): Request = proxy.request()
     override fun isExecuted() = proxy.isExecuted
     override fun isCanceled() = proxy.isCanceled
+
+    abstract fun enqueueImpl(callback: Callback<TOut>)
+    abstract fun cloneImpl(): Call<TOut>
 }
 
 class ResultCall<T>(proxy: Call<T>) : CallDelegate<T, Result<T>>(proxy) {
-    override fun enqueue(callback: Callback<Result<T>>) {
-        proxy.enqueue(object: Callback<T> {
-            override fun onResponse(call: Call<T>, response: Response<T>) {
-                val code = response.code()
-                val result = if (code in 200 until 300) {
-                    val body = response.body()
-                    Result.Success(body)
-                } else {
-                    Result.Failure(code)
-                }
-
-                callback.onResponse(this@ResultCall, Response.success(result))
+    override fun enqueueImpl(callback: Callback<Result<T>>) = proxy.enqueue(object: Callback<T> {
+        override fun onResponse(call: Call<T>, response: Response<T>) {
+            val code = response.code()
+            val result = if (code in 200 until 300) {
+                val body = response.body()
+                Result.Success(body)
+            } else {
+                Result.Failure(code)
             }
 
-            override fun onFailure(call: Call<T>, t: Throwable) {
-                val result = if (t is IOException) {
-                    Result.NetworkError
-                } else {
-                    Result.Failure(null)
-                }
+            callback.onResponse(this@ResultCall, Response.success(result))
+        }
 
-                callback.onResponse(this@ResultCall, Response.success(result))
+        override fun onFailure(call: Call<T>, t: Throwable) {
+            val result = if (t is IOException) {
+                Result.NetworkError
+            } else {
+                Result.Failure(null)
             }
-        })
-    }
+
+            callback.onResponse(this@ResultCall, Response.success(result))
+        }
+    })
+
+    override fun cloneImpl() = ResultCall(proxy.clone())
 }
 
 class ResultAdapter<T>(
