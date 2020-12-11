0375458778362442f5e81e723c64000077283c17
Valeriy Katkov <cppmaster@yandex.ru>
2020-12-11 18:14:57 +0300

    SeparateToolbars example was added

---
+++ SeparateToolbars/app/src/main/java/com/icesmith/separatetoolbars/FirstFragment.kt
@@ -0,0 +1,36 @@
+package com.icesmith.separatetoolbars
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.icesmith.separatetoolbars.databinding.FragmentFirstBinding
+
+class FirstFragment : Fragment() {
+    lateinit var binding: FragmentFirstBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val navController = findNavController()
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
+
+        binding.toSecondButton.setOnClickListener {
+            navController.navigate(R.id.action_firstFragment_to_secondFragment)
+        }
+    }
+}
---
+++ SeparateToolbars/app/src/main/java/com/icesmith/separatetoolbars/SecondFragment.kt
@@ -0,0 +1,32 @@
+package com.icesmith.separatetoolbars
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.icesmith.separatetoolbars.databinding.FragmentSecondBinding
+
+class SecondFragment : Fragment() {
+    lateinit var binding: FragmentSecondBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentSecondBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val navController = findNavController()
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
+    }
+}
